eb64fd98c584a0a9d5d53778f863b4a7b7bb008f
Llck <mandarinu2013njamz@gmail.com>
2017-12-07 08:57:58 +0200

    2

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,39 +1,42 @@
 class StarsController < ApplicationController
 
-def index
-  @stars = Star.all
-end
+  def index
+    @stars = Star.all
+  end
 
-def new
-  @star = Star.new
-end
+  def new
+    @star = Star.new
+  end
 
-def create
-  star = Star.new(star_params)
-  star.save
-  redirect_to star
-end
+  def create
+    star = Star.new(star_params)
+    star.save
+    redirect_to star
+  end
 
-def show
-  @star = Star.find(params[:id])
-end
+  def show
+    @star = Star.find(params[:id])
+  end
 
-def edit
-  @star = Star.find(params[:id])
-end
-def update
-  @star = Star.find(params[:id])
-  @star.update(star_params)
-end
+  def edit
+    @star = Star.find(params[:id])
+  end
 
-def destroy
-  @star = Star.find(params[:id])
-  @star.destroy
-  redirect_to stars_path
-end
+  def update
+    @star = Star.find(params[:id])
+    @star.update(star_params)
+    redirect_to star
+  end
 
-def star_params
-  params.require(:star).permit(:name, :email)
-end
+  def destroy
+    @star = Star.find(params[:id])
+    @star.destroy
+    redirect_to stars_path
+  end
+
+private
+  def star_params
+    params.require(:star).permit(:name, :email)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root 'stars#index'
 
   resources :stars
